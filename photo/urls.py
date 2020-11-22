a125238fae6eeab764a231d6d30747ca98e6156c
kev127 <kelvinkeya125@gmail.com>
2020-11-22 10:28:17 +0300

    add update templates and add media route

--- photo/urls.py
+++ photo/urls.py
@@ -5,3 +5,5 @@ urlpatterns=[
     path('',views.photo,name='photo'),
      path('search/', views.search_profile, name='search_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
